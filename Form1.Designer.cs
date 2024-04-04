7f43edde2fac19c7fed1e96b9abb9b84b70aa37f
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2024-04-03 21:58:59 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ElectrolitosLab1UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMensaje = new System.Windows.Forms.Button();
+            this.pnlEncabezado = new System.Windows.Forms.Panel();
+            this.lblSubtitulo = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.pnlCuerpo = new System.Windows.Forms.Panel();
+            this.pnlEncabezado.SuspendLayout();
+            this.pnlCuerpo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnMensaje
+            // 
+            this.btnMensaje.Location = new System.Drawing.Point(49, 68);
+            this.btnMensaje.Name = "btnMensaje";
+            this.btnMensaje.Size = new System.Drawing.Size(141, 23);
+            this.btnMensaje.TabIndex = 0;
+            this.btnMensaje.Text = "Mensaje";
+            this.btnMensaje.UseVisualStyleBackColor = true;
+            this.btnMensaje.Click += new System.EventHandler(this.btnMensaje_Click);
+            // 
+            // pnlEncabezado
+            // 
+            this.pnlEncabezado.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlEncabezado.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pnlEncabezado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlEncabezado.Controls.Add(this.lblSubtitulo);
+            this.pnlEncabezado.Controls.Add(this.lblTitulo);
+            this.pnlEncabezado.Location = new System.Drawing.Point(12, 12);
+            this.pnlEncabezado.Name = "pnlEncabezado";
+            this.pnlEncabezado.Size = new System.Drawing.Size(560, 56);
+            this.pnlEncabezado.TabIndex = 2;
+            // 
+            // lblSubtitulo
+            // 
+            this.lblSubtitulo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSubtitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubtitulo.Location = new System.Drawing.Point(7, 24);
+            this.lblSubtitulo.Name = "lblSubtitulo";
+            this.lblSubtitulo.Size = new System.Drawing.Size(548, 23);
+            this.lblSubtitulo.TabIndex = 3;
+            this.lblSubtitulo.Text = "Sistema de Remuneraciones";
+            this.lblSubtitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(3, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblTitulo.Size = new System.Drawing.Size(552, 24);
+            this.lblTitulo.TabIndex = 3;
+            this.lblTitulo.Text = "ELECTROLITOS";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlCuerpo
+            // 
+            this.pnlCuerpo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.pnlCuerpo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCuerpo.Controls.Add(this.btnMensaje);
+            this.pnlCuerpo.Location = new System.Drawing.Point(12, 74);
+            this.pnlCuerpo.Name = "pnlCuerpo";
+            this.pnlCuerpo.Size = new System.Drawing.Size(228, 194);
+            this.pnlCuerpo.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.pnlCuerpo);
+            this.Controls.Add(this.pnlEncabezado);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registro";
+            this.pnlEncabezado.ResumeLayout(false);
+            this.pnlCuerpo.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMensaje;
+        private System.Windows.Forms.Panel pnlEncabezado;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblSubtitulo;
+        private System.Windows.Forms.Panel pnlCuerpo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElectrolitosLab1UI
+{
+    public partial class Form1 : Form
+    {
+        // atributos
+        public Form1()
+        {
+            InitializeComponent();
+            this.btnMensaje.Text = "OK";
+        }
+
+
+        private void btnMensaje_Click(object sender, EventArgs e)
+        {
+            int resultado = 0;
+            try
+            {
+                this.btnMensaje.Text = "Cargando ...";
+                resultado = 8 / resultado;
+                MessageBox.Show("Hola C Sharp");
+                    
+                this.btnMensaje.Text = "OK";
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Error = "+ex.Message);
+            }
+        }
+
+      
+    }
+}
